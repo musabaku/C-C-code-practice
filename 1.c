1efb5050e1bf7974849a703bec1de7cde90f4738
MUSAB AHMED KHAN UMAIR <110651555+musabaku@users.noreply.github.com>
2023-01-23 19:19:57 +0300

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,25 +1,50 @@
-#include <stdio.h>
-int fib(int n)
-{
-    if (n == 0)
-    {
-        return 1;
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+    struct node* next;
+    int data;
+}sn;
+void display(sn* ptr){
+    while(ptr != NULL){
+        printf("data : %d\n",ptr->data);
+        ptr=ptr->next;
     }
 
-    else if (n == 1)
-    {
-        return 1;
-    }
-
-    else
-    {
-
-        return fib(n - 1) + fib(n - 2);
-    }
 }
-int main()
-{
-    int a = fib(5);
-    printf("fib is %d", a);
+int reversePrint(sn* head) {
+       if (head == NULL)
+       return ;
+ 
+    // print the list after head node
+    reversePrint(head->next);
+ 
+    // After everything else is printed, print head
+    printf("%d  ", head->data);
+}
+int main(){
+    sn* head;
+    sn* first;
+    sn* second;
+    sn* third;
+
+    head = (sn*)malloc(sizeof(sn));
+    first = (sn*)malloc(sizeof(sn));
+    second = (sn*)malloc(sizeof(sn));
+    third = (sn*)malloc(sizeof(sn));
+
+    head->data = 1;
+    head->next = first;
+
+    first->data = 2;
+    first->next = second;
+
+    second->data = 3;
+    second->next = third;
+
+    third->data = 4;
+    third->next = NULL;
+    // display(head);
+    reversePrint(head);
+
     return 0;
 }
--- 2.c
+++ 2.c
@@ -1,31 +1,51 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-//Complete the following function.
-
-int find_nth_term(int n, int a, int b, int c) {
-  //Write your code here.
-  if(n == 1){
-      return a;
-  }
-  else if (n==2){
-      return b;
-  }
-  else if(n==3){
-      return c;
-  }
-  else{
-      return find_nth_term(n-1,a,b,c)+find_nth_term(n-2,a,b,c)+find_nth_term(n-3,a,b,c);
-  }
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+    struct node* next;
+    int data;
+}sn;
+void display(sn* ptr){
+    while(ptr != NULL){
+        printf("data : %d\n",ptr->data);
+        ptr=ptr->next;
+    }
 }
+sn* insert_begin(sn* head,int n){
+   sn* ptr = (sn*)malloc(sizeof(sn));
+   ptr->next = head;
+//    head=ptr;
+ptr = head;
+   ptr->data = n;
+   return head;
 
-int main() {
-    int n, a, b, c;
-  printf("enter\n");
-    scanf("%d %d %d %d", &n, &a, &b, &c);
-    int ans = find_nth_term(n, a, b, c);
- 
-    printf("%d", ans); 
+}
+sn* insert_middle(){}
+int main(){
+    sn* head;
+    sn* first;
+    sn* second;
+    sn* third;
+
+    head = (sn*)malloc(sizeof(sn));
+    first = (sn*)malloc(sizeof(sn));
+    second = (sn*)malloc(sizeof(sn));
+    third = (sn*)malloc(sizeof(sn));
+
+    head->data = 1;
+    head->next = first;
+
+    first->data = 11;
+    first->next = second;
+
+    second->data = 111;
+    second->next = third;
+
+    third->data = 1111;
+    third->next = NULL;
+    display(head);
+    insert_begin(head,5);
+    display(head);
+
+
     return 0;
 }
