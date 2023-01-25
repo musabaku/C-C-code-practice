619aa0233678cfa4062ef4542d8d60d4868fcf95
MUSAB AHMED KHAN UMAIR <110651555+musabaku@users.noreply.github.com>
2023-01-25 21:03:59 +0300

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,50 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
-typedef struct node{
-    struct node* next;
-    int data;
-}sn;
-void display(sn* ptr){
-    while(ptr != NULL){
-        printf("data : %d\n",ptr->data);
-        ptr=ptr->next;
+#include <stdio.h>
+int fib(int n)
+{
+    if (n == 0)
+    {
+        return 1;
     }
 
+    else if (n == 1)
+    {
+        return 1;
+    }
+
+    else
+    {
+
+        return fib(n - 1) + fib(n - 2);
+    }
 }
-int reversePrint(sn* head) {
-       if (head == NULL)
-       return ;
- 
-    // print the list after head node
-    reversePrint(head->next);
- 
-    // After everything else is printed, print head
-    printf("%d  ", head->data);
-}
-int main(){
-    sn* head;
-    sn* first;
-    sn* second;
-    sn* third;
-
-    head = (sn*)malloc(sizeof(sn));
-    first = (sn*)malloc(sizeof(sn));
-    second = (sn*)malloc(sizeof(sn));
-    third = (sn*)malloc(sizeof(sn));
-
-    head->data = 1;
-    head->next = first;
-
-    first->data = 2;
-    first->next = second;
-
-    second->data = 3;
-    second->next = third;
-
-    third->data = 4;
-    third->next = NULL;
-    // display(head);
-    reversePrint(head);
-
+int main()
+{
+    int a = fib(5);
+    printf("fib is %d", a);
     return 0;
 }
